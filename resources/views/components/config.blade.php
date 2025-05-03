a3c43a567cfb8a2f5a0cd7bb2cef4e609c6cb840
Raihansome12 <raihanahmad3012@gmail.com>
2025-05-03 17:37:54 +0700

    adding sensor status

--- resources/views/components/config.blade.php
+++ resources/views/components/config.blade.php
@@ -5,12 +5,12 @@
     <span class="text-gray-500">|</span>
     <span class="text-gray-900">Real<span class="text-red-600">Time</span></span>
 
-    <div id="streaming" class="hidden flex items-center px-3 py-1 border-2 border-green-500 rounded-full">
+    <div id="streaming" class="flex items-center px-3 py-1/2 border-2 border-green-500 rounded-full">
         <span class="w-2.5 h-2.5 bg-red-600 rounded-full animate-blink mr-2"></span>
         <span class="text-green-500">streaming</span>
     </div>
 
-    <div id="offline" class="hidden px-3 py-1 border-2 border-red-500 rounded-full">
+    <div id="offline" class="hidden px-6 py-1/2 border-2 border-red-500 rounded-full">
         <span class="text-red-500">offline</span>
     </div>
 
